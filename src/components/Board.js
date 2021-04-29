28ef97f200b8b9e463e73db56ac04e6f05cf53f0
Thomas de Groot <degroot12@gmail.com>
2021-04-29 17:47:19 +0200

    cleaned code

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,82 +1,3 @@
-// import React, { useState, useEffect } from 'react';
-// import createBoard from './createBoard'
-// import {revealed} from './reveal'
-
-
-// function Board() {
-//     const [grid, setGrid] = useState([]);
-//     const [nonMineCount, setNonMineCount] = useState(0);
-//     const [mineLocations, setMineLocations] = useState(0);
-
-//     // mounting the component
-//     useEffect(() => {
-//         function freshBoard(){
-//             const newBoard = createBoard(10,10,15);
-//         console.log(newBoard);
-//         setNonMineCount(10*10 - 15);
-//         setMineLocations(newBoard.mineLocation)
-//         setGrid(newBoard.board);
-//         }
-//         freshBoard();
-//     },[]);
-
-//     // right click/ add flag
-//     const updateFlag = (event, x, y) => {
-//         event.preventDefault();
-//         let clonedGrid = JSON.parse(JSON.stringify(grid));
-//         clonedGrid[x][y].flagged = true;
-//         setGrid(clonedGrid);
-//         console.log('right click')
-//     }
-
-//     // Reveal the cell on click
-//     const revealCell = (x,y) => {
-//         let clonedGrid = JSON.parse(JSON.stringify(grid));
-//         if(clonedGrid[x][y].value === 'X'){
-//             alert('found a mine!');
-//             for(let i = 0;i<mineLocations.length;i++){
-//                 clonedGrid[mineLocations[i][0]][mineLocations[i][1]].revealed = true;
-//             }
-//             setGrid(clonedGrid)
-//         } else {
-//             let newRevealedBoard = revealed(clonedGrid, x, y, nonMineCount);
-//             clonedGrid[x][y].revealed = true;
-//             setGrid(newRevealedBoard.arr);
-//             setNonMineCount(newRevealedBoard.newNonMinesCount)
-//         }
-   
-//     }
-
-
-//     if(!grid){
-//         return <div>Loading</div>
-//     }
-
-//     return (
-//         <div >
-//         {grid.map(singleRow => {
-//             return (
-//                 <div className='board'>
-//                     {singleRow.map(singleBlock =>  {
-//                 return (
-//                     <div className='rowsBoard'  onClick={() => revealCell(singleBlock.x, singleBlock.y)} onContextMenu={(event) => updateFlag(event, singleBlock.x, singleBlock.y)}>
-//                         {singleBlock.revealed ? singleBlock.value: ''}
-//                         {/* {singleBlock.value!==0 && singleBlock.value} */}
-//                     </div>
-//                 )
-//             })}
-//             </div>
-//             )
-//         })}
-//         </div>
-//     )
-       
-    
-// }
-
-// export default Board
-
-
 import React, { useEffect, useState } from 'react'
 
 export default function Board() {
@@ -127,8 +48,6 @@ export default function Board() {
             
             boardArr[rowIndex][colIndex].value = 'X';
         }
-        
-        //const nonBombs = numRows * numCols - mines;
 
         for(let i=0;i<numRows ;i++){
             for(let j=0; j<numCols;j++){
@@ -166,10 +85,6 @@ export default function Board() {
         return boardArr
     }
 
-    // function revealZero(row,col){
-
-    // }
-    // const nonBombs = {}
     function handleClick(row, col){  
         const zeroesObj = {};
         let nonMinesCount = nonMines
@@ -266,8 +181,6 @@ export default function Board() {
                     alert('you died')
                 }
                 setGrid(clonedGrid);
-            // console.log('mines', mines);
-            // console.log('nonmines', nonMines)
             if(mines === 0 && nonMines === 0){
                 alert('You won!')
             }
